70c160e18894d2ed4223a4409904bb5f86b9d60f
gural <kubaty03@gmail.com>
2025-06-21 13:39:49 +0200

    Health bar added 1

--- src/main.py
+++ src/main.py
@@ -7,6 +7,13 @@ SIZESCREEN = WIDTH, HEIGHT = 1920, 1080
 screen = pygame.display.set_mode(SIZESCREEN)
 clock = pygame.time.Clock()
 
+#define colours
+BG = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+
 path = os.path.join(os.getcwd(), 'assets/images')
 file_names = os.listdir(path)
 BACKGROUND = pygame.image.load(os.path.join(path, 'background.jpg')).convert()
@@ -20,11 +27,15 @@ for file_name in file_names:
 class Player(pygame.sprite.Sprite):
     def __init__(self, image, cx, cy):
         super().__init__()
-        self.image = image
+        self.image = image.convert_alpha()
         self.rect = self.image.get_rect(center=(cx, cy))
+        self.mask = pygame.mask.from_surface(self.image)
         self.map_x = cx
         self.map_y = cy
-        self.speed = 4
+        self.speed = 6
+        self.maxhp = 80
+        self.curhp = self.maxhp
+        self.armor = 0
 
         self.rect = self.image.get_rect()
 
@@ -48,13 +59,19 @@ class Player(pygame.sprite.Sprite):
     def sync_rect(self, camera_x, camera_y):
         self.rect.centerx = self.map_x - camera_x
         self.rect.centery = self.map_y - camera_y
+    
+    def get_mask_topleft(self):
+        return (self.map_x - self.rect.width // 2, self.map_y - self.rect.height // 2)
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self,image,x,y):
         super().__init__()
         self.image = image
-        self.rect = self.image.get_rect(center=(x,y))
-        self.speed = 1.5
+        self.map_x = x
+        self.map_y = y
+        self.rect = self.image.get_rect(center=(self.map_x, self.map_y))
+        self.mask = pygame.mask.from_surface(self.image)
+        self.speed = 3
         
     def draw(self,surface, camera_x, camera_y):
         surface.blit(self.image,(self.rect.x - camera_x, self.rect.y - camera_y))
@@ -68,8 +85,17 @@ class Enemy(pygame.sprite.Sprite):
             dx /= distance
             dy /= distance
         
-        self.rect.x += dx * self.speed
-        self.rect.y += dy * self.speed
+        self.map_x += dx * self.speed
+        self.map_y += dy * self.speed
+        self.rect.centerx = self.map_x
+        self.rect.centery = self.map_y
+    
+    def sync_rect(self, camera_x, camera_y):
+        self.rect.centerx = self.map_x - camera_x
+        self.rect.centery = self.map_y - camera_y
+
+    def get_mask_topleft(self):
+        return (self.map_x - self.rect.width // 2, self.map_y - self.rect.height // 2)
 
 TILE_SIZE = 32
 tile_images = {
@@ -112,8 +138,18 @@ def draw_tile_map(surface, camera_x, camera_y):
             tile_img = tile_images[tile]
             screen.blit(tile_img, (i * TILE_SIZE + offset_x, j * TILE_SIZE + offset_y))
 
+players = pygame.sprite.Group()
 player = Player(IMAGES['player'],WIDTH//2,HEIGHT//2)
+players.add(player)
 #enemy = Enemy(IMAGES['enemy3'],100,100)
+
+
+def mask_collision(s1, s2):
+    x1, y1 = s1.get_mask_topleft()
+    x2, y2 = s2.get_mask_topleft()
+    offset = (int(x2 - x1), int(y2 - y1))
+    return s1.mask.overlap(s2.mask, offset) is not None
+
 
 camera_x = player.map_x - WIDTH // 2
 camera_y = player.map_y - HEIGHT // 2
@@ -130,15 +166,24 @@ while window_open:
                 window_open = False
         if event.type == pygame.QUIT:
             window_open = False
-    
-    #chodzenie stworow
-    for enemy in enemies:
-        enemy.update((player.map_x, player.map_y))
 
     key_pressed = pygame.key.get_pressed()
     player.update(key_pressed)
 
     player.sync_rect(camera_x, camera_y)
+    
+    # Zanim zaktualizujesz kamerę: kolizje
+    collided_enemies = pygame.sprite.spritecollide(player, enemies, False, pygame.sprite.collide_mask)
+
+    for enemy in enemies:
+        if mask_collision(player, enemy):
+            enemy.update((player.map_x, player.map_y))
+            print("przeciwnik kolizja")
+        else:
+            enemy.update((player.map_x, player.map_y))
+
+
+
     
     # Przesunięcie kamery by "dogonić" gracza
     camera_x += player.rect.centerx - WIDTH // 2
@@ -148,13 +193,23 @@ while window_open:
     player.sync_rect(camera_x, camera_y)
 
     text_surface = my_font.render(f'x: {player.map_x}, y: {player.map_y}', False, (0, 0, 0))
+    text_surface2 = my_font.render(f'enemies: {len(enemies)}', False, (0, 0, 0))
+
 
     screen.fill((0, 0, 0))
     draw_tile_map(screen, camera_x, camera_y)
     screen.blit(text_surface, (0,0))
+    screen.blit(text_surface2, (250,0))
     player.draw(screen)
+
     for enemy in enemies:
         enemy.draw(screen, camera_x, camera_y)
+    
+    pygame.draw.rect(screen, "red", (player.rect.centerx - 40, player.rect.centery + 20, 80, 10))
+    pygame.draw.rect(screen, "green", (player.rect.centerx - 40, player.rect.centery + 20, player.curhp , 10))
+    if player.curhp != 0:
+        player.curhp -= 0.1
+
 
     #aktualizacja okna
     pygame.display.flip()
